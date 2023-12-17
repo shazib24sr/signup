6d238cfd97b080df16734e2f3a26bf7d1726050b
Rana Shazib Ali <98343081+shazib24sr@users.noreply.github.com>
2023-12-17 23:01:27 +0500

    Update login.js

--- src/login.js
+++ src/login.js
@@ -23,7 +23,7 @@ function Login() {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:3306/login', {
+      const response = await fetch('https://admirable-llama-6aa9d1.netlify.app/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
